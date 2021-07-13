b68d4c0b46bb54d7f424ab3ab87efe166a629c97
Chris Pittman <chrispittman73@gmail.com>
2021-07-13 14:32:23 -0500

    Issue 15 markdown editing (#36)
    Change editor to Toast UI editor, to get better Markdown handling
    
    fixes #15

--- web/resources/js/components/WysiwygEditor.vue
+++ web/resources/js/components/WysiwygEditor.vue
@@ -1,178 +1,68 @@
 <template>
-    <div class="quill-editor" ref="editor_container">
-        <slot name="toolbar"></slot>
-        <div ref="editor"></div>
+    <div ref="editor">
+        <div></div>
     </div>
 </template>
 
 <script>
-import _Quill from 'quill'
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
+import Prism from 'prismjs';
+import 'prismjs/themes/prism.css';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js';
 
-const Quill = window.Quill || _Quill
-const defaultOptions = {
-    theme: 'snow',
-    bounds: '.quill-editor',
-    modules: {
-        toolbar: [
-            ['bold', 'italic', 'underline', 'strike'],
-            ['code-block'],
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-            [{ 'script': 'sub' }, { 'script': 'super' }],
-            //[{ 'indent': '-1' }, { 'indent': '+1' }],
-            //[{ 'direction': 'rtl' }],
-            //[{ 'size': ['small', false, 'large', 'huge'] }],
-            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-            [{ 'color': [] }, { 'background': [] }],
-            //[{ 'font': [] }],
-            //[{ 'align': [] }],
-            //['clean'],
-            ['formula', 'link'], //['formula', 'link', 'image']
-            // FIXME: use something like https://github.com/benwinding/quill-image-compress if you let people upload images
-        ]
-    },
-    placeholder: 'Insert text here ...',
-    readOnly: false
-}
-
-// pollfill
-if (typeof Object.assign != 'function') {
-    Object.defineProperty(Object, 'assign', {
-        value (target, varArgs) {
-            if (target == null) {
-                throw new TypeError('Cannot convert undefined or null to object')
-            }
-            const to = Object(target)
-            for (let index = 1; index < arguments.length; index++) {
-                const nextSource = arguments[index]
-                if (nextSource != null) {
-                    for (const nextKey in nextSource) {
-                        if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-                            to[nextKey] = nextSource[nextKey]
-                        }
-                    }
-                }
-            }
-            return to
-        },
-        writable: true,
-        configurable: true
-    })
-}
+import Editor from '@toast-ui/editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
 // export
 export default {
-    name: 'quill-editor',
-    data () {
-        return {
-            _options: {},
-            _content: '',
-            defaultOptions
-        }
-    },
     props: {
-        content: String,
-        value: String,
-        disabled: {
-            type: Boolean,
-            default: false
-        },
-        options: {
-            type: Object,
-            required: false,
-            default: () => ({})
-        },
-        globalOptions: {
-            type: Object,
-            required: false,
-            default: () => ({})
+        value: {
+            type: String,
+            default: "",
         }
     },
-    mounted () {
-        this.initialize()
+    data() {
+        return {
+            current_value: "",
+            editor: null,
+            old_markdown: "",
+        }
     },
-    beforeDestroy () {
-        this.quill = null
-        delete this.quill
+    mounted() {
+        let comp = this;
+        this.editor = new Editor({
+            el: this.$refs.editor.querySelector('div'),
+            height: '300px',
+            initialValue: this.value,
+            initialEditType: 'wysiwyg',
+            previewStyle: 'vertical',
+            usageStatistics: false,
+            plugins: [[codeSyntaxHighlight, { highlighter: Prism }]],
+            toolbarItems: [
+                ['heading', 'bold', 'italic'],
+                ['ul', 'ol'],
+                ['table', 'image', 'link'],
+                ['codeblock'],
+            ],
+            events: {
+                change() {
+                    if (comp.editor === null) {
+                        return;
+                    }
+
+                    // see 'oldMarkdown' comment above
+                    let newMarkdown = comp.editor.getMarkdown();
+                    if (newMarkdown === comp.oldMarkdown) {
+                        return;
+                    }
+                    comp.oldMarkdown = newMarkdown;
+
+                    let html = comp.editor.getHTML();
+                    comp.$emit('input', html);
+                }
+            }
+        });
     },
     methods: {
-        // Init Quill instance
-        initialize () {
-            if (this.$el) {
-
-                // Options
-                this._options = Object.assign({}, this.defaultOptions, this.globalOptions, this.options)
-
-                // Instance
-                this.quill = new Quill(this.$refs.editor, this._options)
-
-                this.quill.enable(false)
-
-                // Set editor content
-                if (this.value || this.content) {
-                    this.quill.pasteHTML(this.value || this.content)
-                }
-
-                // Disabled editor
-                if (!this.disabled) {
-                    this.quill.enable(true)
-                }
-
-                // Mark model as touched if editor lost focus
-                this.quill.on('selection-change', range => {
-                    if (!range) {
-                        this.$emit('blur', this.quill)
-                    } else {
-                        this.$emit('focus', this.quill)
-                    }
-                })
-
-                // Update model if text changes
-                this.quill.on('text-change', (delta, oldDelta, source) => {
-                    let html = this.$refs.editor.children[0].innerHTML
-                    const quill = this.quill
-                    const text = this.quill.getText()
-                    if (html === '<p><br></p>') html = ''
-                    this._content = html
-                    this.$emit('input', this._content)
-                    this.$emit('change', { html, text, quill })
-                })
-
-                // Emit ready event
-                this.$emit('ready', this.quill)
-            }
-        }
-    },
-    watch: {
-        // Watch content change
-        content (newVal, oldVal) {
-            if (this.quill) {
-                if (newVal && newVal !== this._content) {
-                    this._content = newVal
-                    this.quill.pasteHTML(newVal)
-                } else if (!newVal) {
-                    this.quill.setText('')
-                }
-            }
-        },
-        // Watch content change
-        value (newVal, oldVal) {
-            if (this.quill) {
-                if (newVal && newVal !== this._content) {
-                    this._content = newVal
-                    this.quill.pasteHTML(newVal)
-                } else if (!newVal) {
-                    this.quill.setText('')
-                }
-            }
-        },
-        // Watch disabled change
-        disabled (newVal, oldVal) {
-            if (this.quill) {
-                this.quill.enable(!newVal)
-            }
-        }
     }
 }
 </script>
