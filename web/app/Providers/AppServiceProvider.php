e5cbf5047140cc51a294ee14d35fffd7779dde13
Chris Pittman <chrispittman73@gmail.com>
2021-08-02 15:38:19 -0500

    bake in workaround for old mysql versions
    See https://laravel-news.com/laravel-5-4-key-too-long-error for info

--- web/app/Providers/AppServiceProvider.php
+++ web/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // MySQL < 5.7 requires a different default string length
+        if (env('DB_MYSQL56_CUSTOM_STRING_LENGTH')) {
+            Schema::defaultStringLength(191);
+        }
     }
 }
