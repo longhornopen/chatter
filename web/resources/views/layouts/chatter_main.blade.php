05e12c8584efafd6e95b4f15d28bc25f5a43f605
Chris Pittman <chrispittman73@gmail.com>
2021-08-09 08:44:51 -0500

    serve fonts locally
    fixes #38

--- web/resources/views/layouts/chatter_main.blade.php
+++ web/resources/views/layouts/chatter_main.blade.php
@@ -23,10 +23,6 @@
     <script src="{{ mix('js/vendor.js') }}"></script>
     <script src="{{ mix('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
