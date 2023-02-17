b510194ca671d3e48f3023a13b27a8c556b9bcc0
Chris Pittman <chrispittman73@gmail.com>
2023-02-17 10:25:35 -0600

    automatically delete expired tokens from db

--- web/app/Console/Kernel.php
+++ web/app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(SendDigestMails::class)->hourly();
+        $schedule->command('sanctum:prune-expired --hours=24')->daily();
     }
 
     /**
