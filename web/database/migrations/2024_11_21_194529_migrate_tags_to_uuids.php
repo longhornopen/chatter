39f436ff93df2b7d0771ab0f092d20393f742a48
Chris Pittman <chrispittman73@gmail.com>
2024-11-21 15:54:40 -0600

    fix a few migration problems against realistic db data

--- web/database/migrations/2024_11_21_194529_migrate_tags_to_uuids.php
+++ web/database/migrations/2024_11_21_194529_migrate_tags_to_uuids.php
@@ -16,25 +16,29 @@ return new class extends Migration
         // The 'courses' table has a 'post_tags' column, which looks like this:
         // [{"name":"announcement2","bgcolor":"#9A2617","teacher_only":true},{"name":"question","bgcolor":"#093145","teacher_only":false},{"name":"discussion","bgcolor":"#C2571A","teacher_only":false}]
         // Update these and add a 'uuid' attribute to every element of the array with a UUID, and save the result.
-        DB::table('courses')->get()->each(function ($course) {
-            $tags = collect(json_decode($course->post_tags));
-            $tags = $tags->map(function ($tag) {
-                $tag->uuid = Str::uuid();
-                return $tag;
-            });
-            $course->post_tags = $tags->toJson();
-            DB::table('courses')->where('id', $course->id)->update(['post_tags' => $course->post_tags]);
+        DB::table('courses')->orderBy('id')->chunk(100, function ($courses) {
+            foreach ($courses as $course) {
+                $tags = collect(json_decode($course->post_tags));
+                $tags = $tags->map(function ($tag) {
+                    $tag->uuid = Str::uuid();
+                    return $tag;
+                });
+                $course->post_tags = $tags->toJson();
+                DB::table('courses')->where('id', $course->id)->update(['post_tags' => $course->post_tags]);
+            }
         });
 
         // The 'posts' table has a 'tag' column, with the name of the tag, and a 'course_id' pointing to the 'courses' table.
         // If 'tag' is non-empty, update it to contain the UUID of the tag instead of the name.
-        DB::table('posts')->get()->each(function ($post) {
-            if ($post->tag) {
-                $course = DB::table('courses')->where('id', $post->course_id)->first();
-                $tags = collect(json_decode($course->post_tags));
-                $tag = $tags->firstWhere('name', $post->tag);
-                $post->tag = $tag->uuid;
-                DB::table('posts')->where('id', $post->id)->update(['tag' => $post->tag]);
+        DB::table('posts')->orderBy('id')->chunk(100, function ($posts) {
+            foreach ($posts as $post) {
+                if ($post->tag) {
+                    $course = DB::table('courses')->where('id', $post->course_id)->first();
+                    $tags = collect(json_decode($course->post_tags));
+                    $tag = $tags->firstWhere('name', $post->tag);
+                    $post->tag = $tag?->uuid;
+                    DB::table('posts')->where('id', $post->id)->update(['tag' => $post->tag]);
+                }
             }
         });
     }
@@ -44,24 +48,28 @@ return new class extends Migration
      */
     public function down(): void
     {
-        DB::table('posts')->get()->each(function ($post) {
-            if ($post->tag) {
-                $course = DB::table('courses')->where('id', $post->course_id)->first();
-                $tags = collect(json_decode($course->post_tags));
-                $tag = $tags->firstWhere('uuid', $post->tag);
-                $post->tag = $tag->name;
-                DB::table('posts')->where('id', $post->id)->update(['tag' => $post->tag]);
+        DB::table('posts')->orderBy('id')->chunk(100, function ($posts) {
+            foreach ($posts as $post) {
+                if ($post->tag) {
+                    $course = DB::table('courses')->where('id', $post->course_id)->first();
+                    $tags = collect(json_decode($course->post_tags));
+                    $tag = $tags->firstWhere('uuid', $post->tag);
+                    $post->tag = $tag?->name;
+                    DB::table('posts')->where('id', $post->id)->update(['tag' => $post->tag]);
+                }
             }
         });
 
-        DB::table('courses')->get()->each(function ($course) {
-            $tags = collect(json_decode($course->post_tags));
-            $tags = $tags->map(function ($tag) {
-                unset($tag->uuid);
-                return $tag;
-            });
-            $course->post_tags = $tags->toJson();
-            DB::table('courses')->where('id', $course->id)->update(['post_tags' => $course->post_tags]);
+        DB::table('courses')->orderBy('id')->chunk(100, function ($courses) {
+            foreach ($courses as $course) {
+                $tags = collect(json_decode($course->post_tags));
+                $tags = $tags->map(function ($tag) {
+                    unset($tag->uuid);
+                    return $tag;
+                });
+                $course->post_tags = $tags->toJson();
+                DB::table('courses')->where('id', $course->id)->update(['post_tags' => $course->post_tags]);
+            }
         });
     }
 };
